4d290ec37357987bb480fbb899ca84b99efc0482
Yash Sonone <yashsonone567@gmail.com>
2024-05-18 16:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from openai import OpenAI
-import toml
 
 st.set_page_config(layout="wide")
-
-# Read config.toml file
-config = toml.load("config.toml")
-
-# Apply theme from config.toml
-st.set_page_config(
-    page_title="My Streamlit App",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
 st.markdown('<h1 style="color: #F3FF02; font-family:Arial; font-size: 60px; text-align: center;">Football and AI Scouting</h1>', unsafe_allow_html=True)
 st.caption("For player statistics, we initially used the FBref website to scrape all essential data such as name, age, etc. However, recently scraping data has become challenging, so we moved to Fotmob. Despite this, we still need to scrape statistics from the [FBref website](https://fbref.com/en/). Therefore before proceeding, we need to run [colab file](https://colab.research.google.com/drive/1nx0BSmhoenc8joP-EsYNtQQzx2_F9jwa) first to get the statistics data, which will automatically be stored in GitHub as long as the GitHub token is valid (currently for 30 days as of 18/05/2024")
